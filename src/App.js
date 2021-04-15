ee39f6e53fcfd9d83b17f311aed8fc456b4709bf
vineet-codes <vineetwrites@gmail.com>
2021-04-15 11:07:48 +0100

    small mods

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>I was changed by Vineet Singh!!!</p>
+        <p>
+          I was changed by Vineet Singh!!!, now we are integrating travis ci
+        </p>
         <a
           className='App-link'
           href='https://reactjs.org'
